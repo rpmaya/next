4390cc89cb12fb092dc31c2e7c9aeba80b43064d
Ricardo Palacios <rpmaya@gmail.com>
2023-11-09 20:57:48 +0100

    next

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
+import Navbar from '@/app/components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar/>
+        <main className="container mx-auto h-[calc(100vh-7rem)] flex justify-center items-center text-black">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
---
+++ app/users/[id]/page.jsx
@@ -0,0 +1,19 @@
+//Aunque ya tengo el dato, normalmente los APIs me dan más detalle si accedo al elemento concreto, así que lo descargo
+async function getUser(id){
+    const res = await fetch(`https://reqres.in/api/users/${id}`)
+    const data = await res.json()
+    return data.data
+}
+
+async function UserPage({params}) {
+    const user = await getUser(params.id)
+    return (
+        <div className="bg-slate-400 p-10 rounded-md text-white">
+            <img src={user.avatar} alt="" className="m-auto my-4"/>
+            <h3 className="text-3xl font-bold">{user.id} {user.first_name} {user.last_name}</h3>
+            <p>email: {user.email}</p>
+        </div>
+    )
+}
+
+export default UserPage
